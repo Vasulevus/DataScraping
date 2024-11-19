8981d4e22bd35f4d316d48f72a9abf8ee7e90554
Vasulevus <Vasulevus@gmail.com>
2024-11-19 08:04:09 +0200

    Update main.py

--- Lesson5/main.py
+++ Lesson5/main.py
@@ -28,5 +28,33 @@ def write_json(data: list) -> None:
         json.dump(data, f, indent=4)
 
 
+def write_sql(data: list) -> None:
+    filename = "result.db"
+    conn = sqlite3.connect(filename)
+    cursor = conn.cursor()
+
+    SQL = """
+    CREATE TABLE IF NOT EXISTS Jobs(
+    [Id] INTEGER PRIMARY KEY,
+    [Title] TEXT,
+    [Url] TEXT
+    )
+    """
+    cursor.execute(SQL)
+
+    for item in data:
+        cursor.execute(
+            """
+            INSERT INTO Jobs(Title,Url) VALUES(?,?)
+            """, (item[1],item[0])
+        )
+
+    conn.commit()
+    conn.close()
+
+
+
+
 if __name__ == '__main__':
     write_json(get_content2())
+    write_sql(get_content2())
